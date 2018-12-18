5fcc177018751e50e6e932ee673a5f7820304304
janezhu1618 <janezhu@pursuit.org>
2018-12-18 10:29:26 -0500

    5th commit - added app icon

---
+++ NYCPublicLibraryApp/LibraryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LibraryTableViewCell.swift
+//  NYCPublicLibraryApp
+//
+//  Created by Jane Zhu on 12/17/18.
+//  Copyright © 2018 Jane Zhu. All rights reserved.
+//
+
+import UIKit
+
+class LibraryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
